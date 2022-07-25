eb3823fb39a47dcd36d41a2179d80d43cc937e9f
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-25 14:06:21 -0400

    testing json bodies with parser

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
+
+const Database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '1234',
+      name: 'Sally',
+      email: 'Sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
   res.send('this is working.')
+})
+
+app.post('/signin', (req, res) => {
+  if(req.body.email === Database.users[0].email && req.body.password === Database.users[0].password) {
+    res.json('success')
+  } else {
+    res.status(400).json('error logging in')
+  }
+  
 })
 
 
