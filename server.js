c83c4b1af1d1074e6d2056cea366066159c13893
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-26 15:21:04 -0400

    installed bcryptjs

--- server.js
+++ server.js
@@ -53,6 +53,34 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length - 1])
 })
 
+app.get('/proifle/:id', (req, res) => {
+  const { id } = req.params
+  let found = false
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true
+      return res.json(user)
+    } 
+  })
+  if(!found) {
+    res.status(400).json('not found')
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body
+  let found = false
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true
+      user.entries++
+      return res.json(user.entries)
+    } 
+  })
+  if(!found) {
+    res.status(400).json('not found')
+  }
+})
 
 app.listen(3000, () => {
   console.log("Working on 3000")
